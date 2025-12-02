fdc3cf7df66bc009da6c7afef3e53d7081cee2af
maurice920 <maudelab23@gmail.com>
2025-12-02 09:03:20 -0400

    Actualizado

--- obj/Debug/net8.0/BackendCoopSoft.AssemblyInfo.cs
+++ obj/Debug/net8.0/BackendCoopSoft.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendCoopSoft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a54d7097be96ed20b7920a8064f775f238b18132")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+919c2a3a4c0881a4dba68b214a8df989c41d73be")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendCoopSoft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendCoopSoft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
