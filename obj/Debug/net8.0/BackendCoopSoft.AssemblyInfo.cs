9fca1501d4ecc0cac417bb77b875af4c3be8806c
maurice920 <maudelab23@gmail.com>
2025-12-02 15:34:04 -0400

    arreglado huellas

--- obj/Debug/net8.0/BackendCoopSoft.AssemblyInfo.cs
+++ obj/Debug/net8.0/BackendCoopSoft.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackendCoopSoft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d717a3792a6e18ef8190675d50409d9d502718f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1eb72da4c2fae06d786685ead7aba3b79454b32")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackendCoopSoft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackendCoopSoft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
